59e2e189c396a4934481d65eb6e82e933410cb93
nkneeraj <nkneerj641@gmail.com>
2020-09-29 00:56:07 +0530

    [NK]Added UC1: Email starting with abc.

--- src/com/cg/emailValidation/EmailValidation.java
+++ src/com/cg/emailValidation/EmailValidation.java
@@ -1,10 +1,23 @@
 package com.cg.emailValidation;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the Email validator");
+		Scanner sc = new Scanner(System.in);
+		Pattern pincode = Pattern.compile("^abc");
+		System.out.println("Enter the email");
+		String pattern = sc.nextLine();
+		Matcher matcher = pincode.matcher(pattern);
+		boolean isFound = matcher.find();
+		if(isFound)
+			System.out.println("Valid Email");
+		else
+			System.out.println("Not a valid Email");
+		sc.close();
 	}
-
 }
